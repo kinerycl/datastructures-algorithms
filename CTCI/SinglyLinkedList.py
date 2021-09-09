0fe82771a9111d426d905d6d94e4acb06af2175a
Clare Kinery <39098724+kinerycl@users.noreply.github.com>
2021-09-09 15:15:33 -0400

    Create SinglyLinkedList.py

---
+++ CTCI/SinglyLinkedList.py
@@ -0,0 +1,67 @@
+class Node(object):
+    def __init__(self, data, node=None):
+        self.data = data
+        self.next = node
+
+    def __str__(self):
+        return str(self.data)
+
+
+class SinglyLinked(object):
+    def __init__(self, node=None):
+        self.head = node
+
+    def insert_tail(self, node):
+        curr = self.head
+        if curr is None:
+            self.head = node
+        else:
+            while curr.next is not None:
+                curr = curr.next
+
+            curr.next = node
+
+    def insert_head(self, node):
+        node.next = self.head
+        self.head = node
+
+    def remove_head(self):
+        if self.head is not None:
+            if self.head.next is not None:
+                self.head = self.head.next
+            else:
+                self.head = None
+
+    def remove_tail(self):
+        if self.head is not None and self.head.next is not None:
+            prev = self.head
+            curr = self.head.next
+            while curr.next is not None:
+                prev = curr
+                curr = curr.next
+            prev.next = None
+        else:
+            self.head = None
+
+    def find_node(self, data):
+        data = str(data)
+        curr = self.head
+        while curr is not None:
+            if curr.data == data:
+                print("Data found")
+                return curr
+            curr = curr.next
+        else:
+            print("Data not found")
+            return
+
+    def __str__(self):
+        ret = ""
+        node = self.head
+        if node is None:
+            return "Empty List"
+        while node is not None:
+            ret += str(node.data)
+            ret += " "
+            node = node.next
+        return ret
