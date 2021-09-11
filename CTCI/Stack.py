fb614344109c0e58f9630a715bc1170a373f867c
Clare Kinery <39098724+kinerycl@users.noreply.github.com>
2021-09-11 17:14:58 -0400

    Create Stack.py

---
+++ CTCI/Stack.py
@@ -0,0 +1,25 @@
+class Stack(object):
+    def __init__(self):
+        self.data = []
+        self.count = 0
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        self.count -= 1
+        return self.data.pop(0)
+
+    def push(self, item):
+        self.count += 1
+        self.data.append(item)
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.data[0]
+
+    def is_empty(self):
+        if self.count == 0:
+            return True
+        return False
